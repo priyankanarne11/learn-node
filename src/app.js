74b41aeb81aa1e819da751ad019c4e27ed46f7a0
priyankanarne11 <priyachowdary1978@gmail.com>
2025-02-12 12:06:31 +0530

    next() in app.http_methods

--- src/app.js
+++ src/app.js
@@ -1,26 +1,10 @@
 const express = require("express");
 const app = express();
 const useRoutes = require("./use");
+const httpRoutes = require("./http_methods");
 
 app.use(useRoutes);
-
-app.get("/get_id", (req, res) => {
-  res.send({ id: "1234" });
-});
-
-// http://localhost:3000/get_name_by_id?id=1234
-// to access query parameters, we use req.query
-app.get("/get_name_by_id", (req, res) => {
-  console.log(req.query);
-  res.send({ name: "John" });
-});
-
-// http://localhost:3000/get_name_by_id/1234
-// to access path parameters, we use req.params
-app.get("/get_name_by_id/:user_id", (req, res) => {
-  console.log(req.params);
-  res.send({ name: "John" });
-});
+app.use(httpRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ src/http_methods.js
@@ -0,0 +1,41 @@
+express = require("express");
+
+const app = express();
+
+app.get("/get_id", (req, res) => {
+  res.send({ id: "1234" });
+});
+
+//---------------------------------------------------------------
+
+// http://localhost:3000/get_name_by_id?id=1234
+// to access query parameters, we use req.query
+app.get("/get_name_by_id", (req, res) => {
+  console.log(req.query);
+  res.send({ name: "John" });
+});
+
+//---------------------------------------------------------------
+
+// http://localhost:3000/get_name_by_id/1234
+// to access path parameters, we use req.params
+app.get("/get_name_by_id/:user_id", (req, res) => {
+  console.log(req.params);
+  res.send({ name: "John" });
+});
+
+//---------------------------------------------------------------
+
+// next() in below method executes the next matching request
+// the route has to be same
+app.get("/get_id_next", (req, res, next) => {
+  console.log({ id: "1234" });
+  next();
+});
+app.get("/get_id_next", (req, res) => {
+  res.send({ name: "John" });
+});
+
+// each route handler is a middleware
+
+module.exports = app;
